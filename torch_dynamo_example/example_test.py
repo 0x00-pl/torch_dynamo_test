c9abf5ac9e095ce359959698764f8223b9f3d8cf
pl <0x00.pl@gmail.com>
2023-12-20 14:12:06 +0800

    update code

--- torch_dynamo_example/example_test.py
+++ torch_dynamo_example/example_test.py
@@ -5,7 +5,9 @@ from torch import _dynamo as torchdynamo
 
 
 def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
-    print("my_compiler called with fx_graph:\n", gm.graph.print_tabular())
+    # print("my_compiler called with fx_graph:\n", gm.graph.print_tabular())
+    print('=================')
+    print(gm.code)
     return gm.forward
 
 
@@ -13,13 +15,15 @@ def my_compiler(gm: torch.fx.GraphModule, example_inputs: List[torch.Tensor]):
 def toy_example(a, b):
     x = a / (torch.abs(a) + 1)
     if b.sum() < 0:
-        x = x * -1
+        b = b * -1
     return x * b
 
 
 def test():
-    toy_example(torch.randn(3), torch.randn(3))
+    result = toy_example(torch.randn(3), torch.randn(3))
+    print('result is:', result)
 
 
 if __name__ == "__main__":
-    test()
+    for _ in range(10):
+        test()
