41216648d740fbf517d6e1d594b00039bbc5881c
pl <0x00.pl@gmail.com>
2024-01-09 18:09:12 +0800

    add test huggingface model

--- tests/test_dynamo_extract_node.py
+++ tests/test_dynamo_extract_node.py
@@ -5,22 +5,7 @@ from torch import _dynamo as torchdynamo
 from torch.fx.experimental.proxy_tensor import make_fx
 
 from torch_dynamo_test.graph_visiter.modify_node_visitor import ModifyNodeTorchVisitor
-
-
-@torchdynamo.allow_in_graph
-def my_add(a, b):
-    # res = torch.tensor([float(ai) + float(bi) for ai, bi in zip(a, b)])
-    print('running my add.')
-    res = a + b
-    return res
-
-
-@torchdynamo.allow_in_graph
-def my_mul(a, b):
-    # res = torch.tensor([float(ai) + float(bi) for ai, bi in zip(a, b)])
-    print('running my mul.')
-    res = a * b
-    return res
+from torch_dynamo_test.model_pool import huggingface_model
 
 
 class ModifyAddNodeToMyVersionVisitor(ModifyNodeTorchVisitor):
@@ -30,15 +15,43 @@ class ModifyAddNodeToMyVersionVisitor(ModifyNodeTorchVisitor):
         )
         if node.target is torch.ops.aten.add.Tensor:
             print('found torch.add')
-            node.target = my_add
+            node.target = self.my_add
         return node
+
+    @staticmethod
+    @torchdynamo.allow_in_graph
+    def my_add(a, b):
+        # res = torch.tensor([float(ai) + float(bi) for ai, bi in zip(a, b)])
+        print('running my add.')
+        res = a + b
+        return res
 
 
 class ModifyMulNodeToMyVersionVisitor(ModifyNodeTorchVisitor):
     def before_node(self, node: torch.fx.Node):
         if node.target is torch.ops.aten.mul.Tensor:
-            node.target = my_mul
+            print('found torch.mul')
+            node.target = self.my_mul
+        elif node.target is torch.ops.aten.bmm.default:
+            print('found torch.bmm')
+            node.target = self.my_bmm
         return node
+
+    @staticmethod
+    @torchdynamo.allow_in_graph
+    def my_mul(a, b):
+        # res = torch.tensor([float(ai) + float(bi) for ai, bi in zip(a, b)])
+        print('running my mul.')
+        res = a * b
+        return res
+
+    @staticmethod
+    @torchdynamo.allow_in_graph
+    def my_bmm(a, b):
+        # res = torch.tensor([float(ai) + float(bi) for ai, bi in zip(a, b)])
+        print('running my bmm.')
+        res = torch.bmm(a, b)
+        return res
 
 
 def my_compiler(gm: torch.fx.GraphModule, example_inputs: typing.List[torch.Tensor]):
@@ -67,3 +80,14 @@ def test_dynamo_extract_node():
     torchdynamo.reset()
     for _ in range(10):
         toy_example(torch.randn(3), torch.randn(3))
+
+
+@torchdynamo.optimize(my_compiler)
+def run_model(model: torch.nn.Module, example_input: typing.Dict[str, torch.Tensor]):
+    model(**example_input)
+
+
+def test_huggingface_model():
+    for name, model_fn in huggingface_model.model_fn_list.items():
+        model, example_input = model_fn()
+        run_model(model, example_input)
